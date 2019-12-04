fae1f31b3cb8f50833640232fa44f5195fb9367d
pixeluga <anc@bigmir.net>
2019-12-04 12:42:28 +0200

    6. use store

--- app/src/store.js
+++ app/src/store.js
@@ -1,14 +1,4 @@
-function updateState(state, action) {
-    if (action.type == 'INCREMENT') {
-        return { count: state.count + action.amount };
-    } else if (action.type == 'DECREMENT') {
-        return { count: state.count - action.amount };
-    } else {
-        return state;
-    }
-}
-
-class Store {
+export default class Store {
     constructor(updateState, state) {
         this._updateState = updateState;
         this._state = state;
@@ -29,17 +19,3 @@ class Store {
         return () => this._callbacks = this._callbacks.filter(cb => cb !== callback);
     }
 }
-
-const initialState = { count: 0 };
-
-const store = new Store(updateState, initialState);
-
-const incrementAction = { type: 'INCREMENT', amount: 5 };
-const decrementAction = { type: 'DECREMENT', amount: 3 };
-
-const unsubscribe = store.subscribe(() => console.log('State changed', store.state));
-
-store.update(incrementAction);
-store.update(decrementAction);
-store.update({});
-unsubscribe();
